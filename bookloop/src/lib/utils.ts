50783622eaf8a62b5649f971717fab11297913eb
Raghav <kapoorr473@gmail.com>
2026-04-09 15:59:26 -0500

    got rid of extra bookloop dir

--- bookloop/src/lib/utils.ts
+++
@@ -1,6 +0,0 @@
-import { clsx, type ClassValue } from '@nberlette/clsx'
-import { twMerge } from 'tailwind-merge';
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
